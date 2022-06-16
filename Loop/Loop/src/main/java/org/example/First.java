fe8ee090a49798deb3eb3efd0aece5c2f0fd6447
Joao_Godinho <git.qodeless@outlook.com>
2022-06-16 12:25:07 -0300

    Printing numbers from 150 to 300

---
+++ Loop/Loop/src/main/java/org/example/First.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class First {
+    public static void main(String[] args) {
+        for (int i = 150; i < 301; i++) {
+            System.out.println(i);
+        }
+    }
+}
